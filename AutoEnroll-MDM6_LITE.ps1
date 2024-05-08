7123122d0bd859a7f09cb2d13a5574f7ec475c0d
aollivierre <abdullahollivierre@gmail.com>
2024-05-08 11:24:52 -0400

    Create AutoEnroll-MDM6_LITE.ps1

---
+++ AutoEnroll-MDM6_LITE.ps1
@@ -0,0 +1,18 @@
+# Define the log file name based on the hostname
+$hostname = hostname
+$logFileName = "$hostname.txt"
+$logFileFullPath = Join-Path -Path $PSScriptRoot -ChildPath $logFileName
+
+# Start transcript and overwrite if the log file already exists
+Start-Transcript -Path $logFileFullPath -Force
+
+# Your original commands
+New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\MDM" -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\MDM" -Name AutoEnrollMDM -Value 1 -Force
+& "$env:windir\system32\deviceenroller.exe" /c /AutoEnrollMDM
+
+# Optionally, you can log additional information here
+Write-Host "Hostname: $hostname"
+
+# Stop transcript
+Stop-Transcript
